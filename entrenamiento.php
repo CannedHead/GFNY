b6b112b9f2ad0716b6fc0067f9d88c5978b6d3e1
antwort7 <antwort7@gmail.com>
2015-09-24 23:58:36 +0200

    Avance -> Vista Entrenamiento

---
+++ entrenamiento.php
@@ -0,0 +1,30 @@
+<?php include 'header.php';?>  
+
+	<div class="col-xs-12 entrenamiento">
+		<hr>
+		<div class='col-xs-12'>
+			<div class='col-xs-12'>
+				<h1 class='pull-left'>ATENCIÓN</h1>
+			</div>
+			<div class='col-xs-12'>
+				<p>
+					Este esquema es una sugerencia-guía de un plan teórico de entrenamiento, sujeto a las particularidades de la vida cotidiana y del clima; está destinado a ciclistas recreativos que quieren entrenarse con juicio para llegar a la meta del GNFYCOL (148 km) sin fundirse y disfrutando el recorrido. Es para el rutero promedio, que monta al menos tres/cuatro veces por semana y ya tiene cierto fondo en las piernas. Así podrá llegar a la prueba con un óptimo volumen y horas de entrenamiento (+ de 1000). <br><br> 
+					
+					El esquema fue diseñado especialmente para el GFNYCOL - teniendo en cuenta la altimetría del recorrido, la altura de la sabana de Bogotá y demás exigencias de la ruta - por el Entrenador y Metodólogo Deportivo Manuel Mosquera Toncón.
+				</p>
+			</div>
+			<div class ='col-xs-12 col-md-6'>
+                <figure class='photo'>
+                    <div class='square pull-right'></div>
+                    <a href='#' style='background-image:url("http://lorempixel.com/640/720/sports");'></a>
+				</figure>
+			</div>
+			<div class ='col-xs-12 col-md-6'>
+                <figure class='photo'>
+                    <div class='square pull-right'></div>
+                    <a href='#' style='background-image:url("http://lorempixel.com/640/720/sports");'></a>
+				</figure>
+			</div>
+		</div>
+	</div>
+<?php include 'footer.php';?>    

ef4636460798f344c59c04bbdf956274f7210c8e
antwort7 <antwort7@gmail.com>
2015-09-25 15:28:56 +0200

    reglas DONE

--- reglas.php
+++ reglas.php
@@ -84,8 +84,153 @@
 					(2) Corrales Competitivos:<br><br>
 
 					(2.1) Corredores con licencia máster activa en varios corrales de acuerdo con su ranking.<br>
-					Requerimiento mínimo: Categ. 1-3 Hombres, Categ. 1-2 Mujeres, Categ. 1-2 Master hombres, Categ. 1-2 Master Mujeres.<br><br>
+					Requerimiento mínimo: Categ. 1-3 Hombres, Categ. 1-2 Mujeres, Categ. 1-2 Master hombres, Categ. 1-2 Master mujeres y su equivalente internacional. GFNY se reserva el derecho de excluir las categorias bajas en caso de no haber mucha demanda.<br><br>
 
+					(2.2) El primer 10% de cada grupo de edad de las 3 ediciones previas del GFNY.<br><br>
+
+					(3) Categoria general GF, de acuerdo con su grupo de edad (de menor a mayor).<br><br>
+
+					(4) Atletas discapacitados con bicicleta de mano.<br><br>
+
+					Los corredores deben empezar en el corral designado, si bien están autorizados a empezar en un corral más atrás del que les fue designado. Si un ciclista intenta ingresar en un corral más adelante que aquel al que ha sido asignado, será descalificado.<br>
+
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>5. Competencia</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					5.1. Todos los participantes son cronometrados electrónicamente mediante chips desde la partida hasta la llegada.<br>
+
+					-Los primeros 20 corredores en la categoría general son clasificados por orden de llegada. El jurado puede cambiar este número en caso de ser necesario si hay grupos llegando alrededor del puesto número 20.<br>
+
+					-Todos los demás participantes se clasifican por tiempo neto (mediante chip).<br>
+
+					-Solo aquellos que terminen la totalidad del recorrido de GFNYCOL son elegibles para clasificación y premios. GFNYCOL se reserva el derecho, a su discreción exclusiva, de cambiar estas reglas si lo considera pertinente.<br><br>
+					
+					5.2. Categorías:<br><br>
+
+					General h/m 18-39 h/m (hombres/mujeres)<br><br>
+
+					2. 40-44 h/m<br><br>
+
+					3. 45-49 h/m<br><br>
+
+					4. 50-54 h/m<br><br>
+
+					6. 55-59 h/m<br><br>
+
+					7. 60-64 h/m<br><br>
+
+					8. Más de 65 m<br><br>
+
+					9. 65-69 h 10. Más de 70 h<br><br>
+
+					11. Menos de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control. Se permite y se promueve la asistencia externa entre compañeros de equipo. No se permiten instrumentos para empujar o arrastrar.<br><br>
+
+					12. Más de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control. Se permite y se promueve la asistencia externa entre compañeros de equipo. No se permiten instrumentos para empujar o arrastrar.<br><br>
+
+					13. Equipo de cuatro (cualquier sexo): se suman los tiempos de los cuatro ciclistas más veloces de un equipo. 5.3. Los premios se entregarán a los primeros tres en cada categoría. La distribución de los premios queda a la discreción exclusiva de GFNYCOL. Los premios que no son reclamados durante la premiación serán repartidos entre otros ciclistas.<br><br>
+
+					5.4. Los reclamos sobre el orden de llegada serán examinados y resueltos por el juez principal, cuya decisión es final e inapelable. <br>
+
+					- Es responsabilidad del ciclista familiarizarse con el reglamento del evento y con el recorrido antes de comenzar. <br>
+
+					- Tomar rutas alternas o atajos conllevará la descalificación de la competencia y exclusión de futuros eventos de GFNYCOL.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>6. Agresiones</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					6.1. No se permitirá la falta de respeto de los ciclistas hacia los directores, funcionarios, personal, ciclistas, voluntarios o espectadores de la carrera.<br><br>
+
+					6.2. No se permitirá el uso de lenguaje o conductas groseras u ofensivas durante GFNYCOL.<br><br>
+
+					6.3. Ningún ciclista podrá atacar (intento ilícito, asociado con la capacidad de ocasionar una lesión violenta a otra persona) o agredir físicamente (uso voluntario o involuntario de fuerza o violencia sobre otra persona) a alguien relacionado con GFNYCOL (incluyendo, entre otros, corredores, espectadores, empleados públicos, voluntarios, personal, etc.).<br><br>
+
+					6.4. Cualquiera de los actos anteriores conllevará la descalificación y exclusión vitalicia de los eventos del Gran Fondo New York Inc.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>7. Conducta</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					7.1. Los ciclistas deben obedecer todas las normas de tránsito, incluyendo semáforos y señales de PARE, a menos que se encuentren sobre-escritas por la policía. Los infractores serán multados por las autoridades locales respectivas, descalificados y retirados del evento.<br><br>
+
+					7.2. Los ciclistas no deberán realizar, de manera intencional o accidental, ningún movimiento abrupto que interfiera con el progreso de otro ciclista.<br><br>
+
+					7.3. Cualquier ciclista que pueda representar un peligro para los otros ciclistas podrá ser descalificado por GFNYCOL antes, durante, o después de la competencia.<br><br>
+
+					7.4. Está prohibido empujar, halar o cualquier otra ayuda física entre los ciclistas de todas las categorías, excepto las categorías Él y Ella, en las cuales se promueve empujar o halar. No se permite el uso de instrumentos para hallar o empujar.<br><br>
+
+					7.5. No están permitidos los vehículos de equipos ni vehículos de apoyo personal.<br><br>
+
+					7.6. Cualquier ciclista que encuentre en su camino a otro en el suelo en visible estado de dificultad deberá detenerse y auxiliarlo, a menos que haya un mínimo de tres personas ayudando. El ciclista podrá continuar si a) la víctima se encuentra bien, o b) ya ha llegado el personal médico. Recuerde que hoy puede ser otro, mañana podrá ser usted.<br><br>
+
+					7.7. Cualquier falta de conducta relacionada con lo anterior puede resultar en la descalificación y prohibición vitalicia de participar en los eventos del Gran Fondo New York Inc.<br><br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>8. Medio ambiente</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					GFNYCOL está comprometido con la protección del medio ambiente. Está prohibido arrojar basura o alimentos afuera de los contenedores de basura designados en las estaciones de asistencia o "eco-zonas", lo cual puede llevar a la descalificación y prohibición vitalicia de participar en los eventos del Gran Fondo New York Inc. <br>
+					También está prohibido orinar en público y puede llevar a la descalificación y prohibición vitalicia de participar en los eventos del Gran Fondo New York Inc.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>9. Sustancias estimulantes del rendimiento deportivo</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					9.1 GFNYCOL está comprometido con un deporte libre de drogas. Las sustancias estimulantes del rendimiento deportivo (PED por sus siglas en inglés) están prohibidas tanto en la preparación como durante la competencia GFNYCOL. GFNYCOL se reserva el derecho de hacer cumplir los controles de dopaje antes, durante y después del evento.<br><br>
+
+					9.2 Toda violación a las Normas de la Asociación Mundial Antidopaje (AMA) con llevará a la descalificación, retiro de premios y a la prohibición vitalicia para participar en los eventos Gran Fondo de Nueva York, Inc. GFNYCOL comparte los resultados positivos de controles de dopaje con otras organizaciones de eventos, así como con el público en general una vez que la USADA determine que hubo una violación de las reglas.<br><br>
+
+					9.3 Si un ciclista se niega a ser sometido a un control, será excluido de la competencia.<br><br>
+
+					9.4 Todo ciclista que dé positivo durante GFNYCOL tendrá que reembolsar el costo del control de dopaje. De igual manera, dicho ciclista deberá reembolsar a GFNYCOL cualquier daño o perjuicio causado a su reputación como consecuencia de su resultado positivo en el control de dopaje. Si el ciclista es parte de un equipo que participe en GFNYCOL, el equipo puede ser considerado responsable de este daño o perjuicio.<br><br>
+
+					A los participantes GFNYCOL se les recomienda encarecidamente que permanezcan informados acerca de las pruebas PED y PED.<br><br>
+
+					GFNYCOL recomienda la lectura y consulta del Manual del Atleta de la USADA.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>10. Aceptación de riesgo</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					Todos aquellos que corran o compitan bajo estas reglas lo hacen bajo su propio riesgo.<br>
+
+					 Ni GFNYCOL ni ninguno de sus funcionarios acepta responsabilidad alguna con respecto a cualquier pérdida o daño sufrido o causado por cualquier persona que corra o compita en GFNYCOL.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>11. Cambios de reglas</h2>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					A su entera discreción, GFNYCOL podrá alterar o cambiar cualquier regla si lo llegare a considerar necesario.<br>
 				</p>
 			</div>
 

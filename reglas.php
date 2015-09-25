2dbd1842b5b5a8a98008e1715ebc1aece45b0154
antwort7 <antwort7@gmail.com>
2015-09-25 14:04:40 +0200

    rey & reina

---
+++ reglas.php
@@ -0,0 +1,57 @@
+<?php include 'header.php';?>  
+
+	<div class="col-xs-12 reglas">
+		<hr class='visible-xs'>
+		<div class='col-xs-12'>
+			
+			<div class ='hidden-xs col-md-12'>
+				<img class="img-responsive" src="http://lorempixel.com/640/132/sports/">
+			</div>
+			
+			<div class='col-xs-12 visible-xs'>
+				<h1 class='pull-left'>REGLAS:</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					1. GFNY COLOMBIA ("GFNYCOL") es una competencia ciclística en la que los ciclistas son cronometrados individualmente.<br>
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>2. Inscripción</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					2.1. La inscripción a GFNYCOL está abierta a ciclistas mayores de 18 años. El registro se hace online en: www.gfny-colombia.com. Las inscripciones online se cierran el 16 de Abril; GFNYCOL se reserva todos los derechos sobre el número de inscripciones y fechas de cierre.<br><br>
+
+					2.2. No se permite la inscripción a ciclistas sansionados por una federación a causa de doping.<br><br>
+
+					2.3. Los ciclistas que hayan tenido licencia profesional (UCI) en los utlimos cinco años prueden tomar parte en la carrera sin competir. En ese caso no deberán interrumpir la ruta de los ciclistas participantes en la carrera.<br><br>
+
+					2.4. Los ciclistas deberán asegurarse de que su condición de salud sea apta para la competencia.<br><br>
+
+					2.5. Los ciclistas deben poseer las pólizas de seguros necesarias. GFNYCOL podrá ofrecer seguros adicionales a discreción propia.<br><br>
+
+					2.6. El valor de las inscripciones no es reembolsable, transferible o diferible. No se harán devoluciones en caso de descalificación.<br><br>
+
+					2.7. GFNYCOL se reserva el derecho, a entre discreción, de negar la inscripción, revocar la solicitud de inscripción de cualquier persona en cualquier momentoo a descalificar a cualquier corredor del evento.<br>
+
+				</p>
+			</div>
+
+			<div class='col-xs-12'>
+				<h2 class='pull-left'>3. Equipo</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+				3.1. Estará permitida toda bicicleta que esté en condiciones técnicas de circular.
+
+				</p>
+			</div>
+
+		</div>
+	</div>
+<?php include 'footer.php';?>    

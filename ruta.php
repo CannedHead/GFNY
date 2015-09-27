487c5746db5c343bc78e17dcdb229f9ccc2e53ff
antwort7 <antwort7@gmail.com>
2015-09-27 23:32:35 +0200

    vista rutas mobile -> DONE

--- ruta.php
+++ ruta.php
@@ -1,2 +1,120 @@
-<?php include 'header.php';?>    
+<?php include 'header.php';?>  
+
+	<div class="col-xs-12 ruta">
+		<hr class='visible-xs'>
+		<div class='col-xs-12'>
+    		<!-- Swiper -->
+		    <div class="swiper-container col-xs-12 visible-xs">
+		        <div class="swiper-wrapper">
+		            <div class="swiper-slide">
+						<img class="img-responsive" src="http://lorempixel.com/429/623/sports/">
+		                <div class="title" style='height:50vh;'>
+		                    <h1>RECORRIDO</h1>
+		                    <p>En Colombia casi todos montan en bicicleta, sean jardineros o presidentes. Esta es la tierra de algunos de los mejores escaladores del mundo, y Bogotá y su sabana, el patio de recreo donde varios campeones han formado sus piernas y forjado su tenaz carácter.</p>
+		                    <a href="#" class="control-next hidden-xs">
+		                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		                      <span class="sr-only">Siguiente</span>
+		                    </a>
+		                </div>
+		            </div>
+		            <div class="swiper-slide">
+		            	<img class="img-responsive" src="http://lorempixel.com/429/623/sports/">
+		                <div class="title" style='height:78vh;'>
+		                    <h2> RUTA DEL <h2>
+		                    <h1>GRAN FONDO</h1>
+		                    <p>
+								La ruta del Gran Fondo lo sorprenderá en más de un momento. Terreno montañoso de variado perfil de inclinación y diversos falsos planos, con el punto más exigente en los 11km de subida a la Cuchilla, ubicada en el Páramo de Guasca (3.365 msnm), crono escalada que coronará al Rey y Reina de la montaña. Por todo esto, los locales celebrarán su llegada a la meta como si fuera un campeón de etapa del Tour de Francia. 	                    	
+		                    </p>
+		                    <a href="#" class="control-next hidden-xs">
+		                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		                      <span class="sr-only">Siguiente</span>
+		                    </a>
+		                </div>
+		            </div>
+		            <div class="swiper-slide">
+		            	<img class="img-responsive" src="http://lorempixel.com/429/623/sports/">
+		                <div class="title" style='height:68vh;'>
+		                    <h2> RUTA DEL <h2>
+		                    <h1>MEDIO FONDO</h1>
+		                    <p>
+								El Medio Fondo transcurre sobre el mismo montañoso e impactante camino que toma	el Gran Fondo, pero en vez de 148km cubre 98km, que también son exigentes aunque no incluyan el crono escalada, que es uno de los tramos más exigentes del GFNY Colombia, ideal para los más experimentados y competitivos participantes.                   	
+		                    </p>
+		                    <a href="#" class="control-next hidden-xs">
+		                      <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+		                      <span class="sr-only">Siguiente</span>
+		                    </a>
+		                </div>
+		            </div>
+		        </div>
+		    </div>	
+
+			<div class='col-xs-12'>
+				<h1 class='pull-left'>RECORRIDO</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					En Colombia casi todos montan en bicicleta, sean jardineros o presidentes. Esta es la tierra de algunos de los mejores escaladores del mundo, y Bogotá y su sabana, el patio de recreo donde varios campeones han formado sus piernas y forjado su tenaz carácter. La ruta del Gran Fondo lo sorprenderá en más de un momento. La salida será en las afueras de Bogotá, en inmediaciones de La Calera, población ubicada a 11km de la ciudad. A lo largo del camino los participantes serán retados por un terreno montañoso de variado perfil de inclinación y diversos falsos planos, con el punto más exigente en los 11km de subida a la Cuchilla, ubicada en el Páramo de Guasca (3.365 msnm), crono escalada que coronará al Rey y Reina de la montaña. Por todo esto, los locales cele-
+					brarán su llegada a la meta como si fuera todo un campeón de etapa del Tour de Francia.
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-12'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+	<div class='gray'></div>	
+
+	<div class="col-xs-12 ruta">
+		<div class='col-xs-12'>
+			<div class='col-xs-12'>
+				<h2> RUTA DEL </h2>
+				<h1 class='pull-left'>GRAN FONDO</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					En Colombia casi todos montan en bicicleta, sean jardineros o presidentes. Esta es la tierra de algunos de los mejores escaladores del mundo, y Bogotá y su sabana, el patio de recreo donde varios campeones han formado sus piernas y forjado su tenaz carácter. La ruta del Gran Fondo lo sorprenderá en más de un momento. La salida será en las afueras de Bogotá, en inmediaciones de La Calera, población ubicada a 11km de la ciudad. A lo largo del camino los participantes serán retados por un terreno montañoso de variado perfil de inclinación y diversos falsos planos, con el punto más exigente en los 11km de subida a la Cuchilla, ubicada en el Páramo de Guasca (3.365 msnm), crono escalada que coronará al Rey y Reina de la montaña. Por todo esto, los locales cele-
+					brarán su llegada a la meta como si fuera todo un campeón de etapa del Tour de Francia.
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-12'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+	<div class='gray'></div>	
+
+	<div class="col-xs-12 ruta" style='margin-bottom:10%'>
+		<div class='col-xs-12'>
+			<div class='col-xs-12'>
+				<h2> RUTA DEL </h2>
+				<h1 class='pull-left'>MEDIO FONDO</h1>
+			</div>
+			
+			<div class='col-xs-12'>
+				<p>
+					En Colombia casi todos montan en bicicleta, sean jardineros o presidentes. Esta es la tierra de algunos de los mejores escaladores del mundo, y Bogotá y su sabana, el patio de recreo donde varios campeones han formado sus piernas y forjado su tenaz carácter. La ruta del Gran Fondo lo sorprenderá en más de un momento. La salida será en las afueras de Bogotá, en inmediaciones de La Calera, población ubicada a 11km de la ciudad. A lo largo del camino los participantes serán retados por un terreno montañoso de variado perfil de inclinación y diversos falsos planos, con el punto más exigente en los 11km de subida a la Cuchilla, ubicada en el Páramo de Guasca (3.365 msnm), crono escalada que coronará al Rey y Reina de la montaña. Por todo esto, los locales cele-
+					brarán su llegada a la meta como si fuera todo un campeón de etapa del Tour de Francia.
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-12'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+    <!-- Swiper JS -->
+    <script src="js/swiper.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+    var swiper = new Swiper('.swiper-container');
+    </script>
+
 <?php include 'footer.php';?>    

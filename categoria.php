37759e5de33db955b34722fa7f79c7261f7462d5
jdflorez39 <jd.florez39@gmail.com>
2015-09-28 00:53:08 -0500

    avance categoria

---
+++ categoria.php
@@ -0,0 +1,70 @@
+<?php include 'header.php';?>  
+	<div class="container">
+		<p>Las personas pueden inscribirse individualmente o también como miembros de un equipo para participar en la Competencia por</p>
+	</div>
+	<div class="row">
+	<a href="#" class="col-xs-12 col-sm-6 categoria">
+		<div class="categoria-wrapper">
+		    <img class="img-responsive" src="img/categoria3.jpg">
+		    <h2 class="text-center">Equipos GNFY COLOMBIA</h2>
+	    </div>
+  	</a>
+	<a href="#" class="col-xs-12 col-sm-6 categoria">
+		<div class="categoria-wrapper">
+		    <img class="img-responsive" src="img/categoria1.jpg">
+		    <h2 class="text-center">Corrales de partida</h2>
+	    </div>
+  	</a>
+  	</div>
+	<a href="#" class="col-xs-12 col-sm-6 categoria">
+		<div class="categoria-wrapper">
+		    <img class="img-responsive" src="img/categoria4.jpg">
+		    <h2 class="text-center">Competencia por equipos</h2>
+	    </div>
+  	</a>
+	<a href="#" class="col-xs-12 col-sm-6 categoria">
+		<div class="categoria-wrapper">
+		    <img class="img-responsive" src="img/categoria2.jpg">
+		    <h2 class="text-center">Categoria él-ella</h2>
+	    </div>
+  	</a>
+  	<div class="container categorias">
+  		<h2>Equipos GNFY COLOMBIA</h2>
+  		<div class="row">
+	  		<p class="col-sm-6">
+	  		   1. 18-39 h/m (hombres/mujeres)<br>
+			   2. 40-44 h/m<br>
+			   3. 45-49 h/m<br>
+			   4. 50-54 h/m<br>
+			   5. 55-59 h/m<br>
+			   6. 60-64 h/m<br>
+			   7. Más de 65 m<br>
+			   8. 65-69 h<br>
+			   9. Más de 70 h<br>
+			   10. Menos de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control. Se permite y se promueve la asistencia externa entre compañeros de equipo. No se permiten instrumentos para empujar o arrastrar.<br>
+			   11. Más de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control.</p>
+			<div class="col-sm-6 hidden-xs">
+				<img class="img-responsive" src="http://lorempixel.com/400/274">
+			</div>
+		</div>
+  		<h2>Corrales de partida</h2>
+  		<p>En GFNY COLOMBIA el propósito ubicarlo en el corral de acuerdo con su condición física. De esa manera usted estará rodeado de ciclistas que corren en su mismo nivel desde el principio, lo que mejora su experiencia.</p>
+  		<div class="row">
+	  		<p class="col-sm-6">
+				A. Clasificación de los ciclistas para partida corrales delanteros:<br><br>
+				1. Corredores inscritos en VIP; corredores que hayan sido campeones o entraron entre los primeros 5 en ediciones previas del GFNY (Gene-
+				ral, por edad o equipo); invitados especiales.<br>
+				2. Los ciclistas que terminaron dentro del primer 10% de su Grupo de Edad en GFNY en 2012-2014.<br><br>
+				Consulte en: https://granfondony.com/results/ la lista completa de los ciclistas calificados. Si su nombre aparece en esa lista y se ha regis-
+				trado, entonces está listo y no deberá hacer nada más al respecto.<br><br>
+				3. Los ciclistas que no están calificados pero que poseen una licencia de la USAC (o su equivalente internacional): hombres de las Catego-
+				rías 1-3 ( Masters 1-2) y Mujeres de las Categorías 1-2 (Masters 1) y que se hayan inscrito antes del 16 de marzo de 2015. Por favor, envíe a info@gfnycolombia.com una fotocopia de su licencia actual, así como pruebas de que ha estado corriendo en su categoría en 2013 o 2014. GFNYCOL se reserva el derecho de limitar el corral delantero para Hombres de Categoría 3 y Mujeres de Categoría 2 o categorías superiores si hay limitaciones de espacio.<br><br>
+				B. Después del corral delantero, los ciclistas serán alienados de acuerdo con su grupo de edad. <br>Si desea empezar con un amigo, usted podrá ubicarse más atrás, pero nunca más adelante.<br> Todos los equipos Él y Ella serán ubicados en el segundo corral.</p>
+			<div class="col-sm-6 hidden-xs">
+				<img class="img-responsive" src="http://lorempixel.com/400/275">
+			</div>
+		</div>
+  		<h2>Competencia por equipos</h2>
+  		<h2>Categoria él-ella</h2>
+  	</div>
+<?php include 'footer.php';?>  
--- rey-reina.php
+++ rey-reina.php
@@ -11,7 +11,7 @@
 					El Rey y la Reina de la Montaña merecerán su corona cuando alcancen la cima de Cuchilla, tras una cronoescalada de 11km. Esta ruta ofrece un espectacular vista e inclinaciones del 5%-7% y, en algunos trechos, rampas de mas de 10%. Los participantes en el Gran Fondo que logren el mejor tiempo, en categoría femenina y masculina, se llevarán el premio.<br>
 				</p>
 			</div>
-			<div class ='col-xs-12 col-md-12'>
+			<div class ='col-xs-12'>
 				<img class="img-responsive" src="http://lorempixel.com/640/360/sports/">
 			</div>
 		</div>

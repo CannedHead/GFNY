755203c185a1092c4849096e5a753a699abc2e5b
antwort7 <antwort7@gmail.com>
2015-10-02 17:45:54 +0200

    vista categorias mobile DONE

---
+++ categorias.php
@@ -0,0 +1,192 @@
+<?php include 'header.php';?>  
+
+	<div class="col-xs-12 categorias">
+		<hr class='visible-xs'>
+		<div class='col-xs-12'>
+    		<!-- Swiper for mobile -->
+		    <div class="swiper-container col-xs-12 visible-xs">
+		        <div class="swiper-wrapper">
+		            <div class="swiper-slide">
+						<img class="img-responsive" src="http://lorempixel.com/429/530/sports/1">
+		                <div class="title-xs" >
+		                    <h2> Equipos <h2>
+		                    <h1>GNFY COLOMBIA.</h1>
+		                </div>
+		            </div>
+		            <div class="swiper-slide">
+		            	<img class="img-responsive" src="http://lorempixel.com/429/530/sports/2">
+		                <div class="title-xs" >
+		                    <h2> CORRALES<h2>
+		                    <h1>DE PARTIDA</h1>
+		                </div>
+		            </div>
+		            <div class="swiper-slide">
+		            	<img class="img-responsive" src="http://lorempixel.com/429/530/sports/3">
+		                <div class="title-xs">
+		                    <h2> COMPETENCIA <h2>
+		                    <h1>POR EQUIPOS</h1>
+		                </div>
+		            </div>
+		            <div class="swiper-slide">
+		            	<img class="img-responsive" src="http://lorempixel.com/429/530/sports/4">
+		                <div class="title-xs">
+		                    <h2> CATEGORIA<h2>
+		                    <h1>EL-ELLA</h1>
+		                </div>
+		            </div>
+		        </div>
+		    </div>	
+
+    		<!-- Div for web -->
+		    <div class="col-md-12 hidden-xs" style='margin-top: 1.2%'>
+				<div class="container">
+					<p>Las personas pueden inscribirse individualmente o también como miembros de un equipo para participar en la Competencia por</p>
+				</div>
+				<div class="row">
+					<a href="#" class="col-xs-12 col-sm-6 categoria">
+						<div class="categoria-wrapper">
+						    <img class="img-responsive" src="img/categoria3.jpg">
+						    <h3 class="text-center">Equipos GNFY COLOMBIA</h3>
+					    </div>
+				  	</a>
+					<a href="#" class="col-xs-12 col-sm-6 categoria">
+						<div class="categoria-wrapper">
+						    <img class="img-responsive" src="img/categoria1.jpg">
+						    <h3 class="text-center">Corrales de partida</h3>
+					    </div>
+				  	</a>
+			  	</div>
+				<a href="#" class="col-xs-12 col-sm-6 categoria">
+					<div class="categoria-wrapper">
+					    <img class="img-responsive" src="img/categoria4.jpg">
+					    <h3 class="text-center">Competencia por equipos</h3>
+				    </div>
+			  	</a>
+				<a href="#" class="col-xs-12 col-sm-6 categoria">
+					<div class="categoria-wrapper">
+					    <img class="img-responsive" src="img/categoria2.jpg">
+					    <h3 class="text-center">Categoria él-ella</h3>
+				    </div>
+			  	</a>
+		    </div>
+
+			<div class='col-xs-12 col-md-6 categorias-top'>
+				<h2>Equipos</h2>
+				<h1>GNFY COLOMBIA.</h1>
+				<p class='ruta-p'>
+		  		   1. 18-39 h/m (hombres/mujeres)<br>
+				   2. 40-44 h/m<br>
+				   3. 45-49 h/m<br>
+				   4. 50-54 h/m<br>
+				   5. 55-59 h/m<br>
+				   6. 60-64 h/m<br>
+				   7. Más de 65 m<br>
+				   8. 65-69 h<br>
+				   9. Más de 70 h<br>
+				   10. Menos de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control. Se permite y se promueve la asistencia externa entre compañeros de equipo. No se permiten instrumentos para empujar o arrastrar.<br>
+				   11. Más de 90 años la suma de edades Él y Ella: se suman los tiempos de un equipo de género mixto. Los ciclistas deben estar a menos de 30 segundos entre sí al pasar por el punto de control.
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-6 hidden-xs categorias-top'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+
+		</div>
+	</div>
+
+	<div class='gray'></div>	
+
+	<div class="col-xs-12 categorias">
+		<div class='col-xs-12'>
+		
+			<div class='col-xs-12 col-md-6'>
+				<h2> CORRALES </h2>
+				<h1>DE PARTIDA</h1>
+				<p class='ruta-p'>
+					En GFNY COLOMBIA el propósito ubicarlo en el corral de acuerdo con su condición física. De esa manera usted estará rodeado de ciclistas que corren en su mismo nivel desde el principio, lo que mejora su experiencia.<br><br>
+					
+					A. Clasicación de los ciclistas para partida corrales delanteros:<br><br>
+
+					1. Corredores inscritos en VIP; corredores que hayan sido campeones o entraron entre los primeros 5 en ediciones previas del GFNY (General, por edad o equipo); invitados especiales.<br>
+					2. Los ciclistas que terminaron dentro del primer 10% de su Grupo de Edad en GFNY en 2012-2014.<br><br>
+
+					Consulte en: https://granfondony.com/results/ la lista completa de los ciclistas calicados. Si su nombre aparece en esa lista y se ha registrado, entonces está listo y no deberá hacer nada más al respecto.<br><br>
+
+					3. Los ciclistas que no están calicados pero que poseen una licencia de la USAC (o su equivalente internacional): hombres de las Categorías 1-3 ( Masters 1-2) y Mujeres de las Categorías 1-2 (Masters 1) y que se hayan inscrito antes del 16 de marzo de 2015.<br><br>
+
+					Por favor, envíe a info@gfnycolombia.com una fotocopia de su licencia actual, así como pruebas de que ha estado corriendo en su categoría en 2013 o 2014. GFNYCOL se reserva el derecho de limitar el corral delantero para Hombres de Categoría 3 y Mujeres de Categoría 2 o categorías superiores si hay limitaciones de espacio. <br><br>
+
+					B. Después del corral delantero, los ciclistas serán alienados de acuerdo con su grupo de edad. Si desea empezar con un amigo, usted podrá ubicarse más atrás, pero nunca más adelante. Todos los equipos Él y Ella serán ubicados en el segundo corral.<br>
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-6 hidden-xs'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+	<div class='gray'></div>	
+
+	<div class="col-xs-12 categorias">
+		<div class='col-xs-12'>
+			
+			<div class='col-xs-12 col-md-6 '>
+				<h2> COMPETENCIA</h2>
+				<h1>POR EQUIPOS</h1>
+				<p class='ruta-p'>
+					Las personas se pueden registrar como miembros de un equipo para participar en la competencia por Equipos GFNYCOL.<br><br>
+
+					• Para determinar el equipo más veloz, se suman y clasifican los cuatro mejores tiempos de un equipo.<br><br>
+
+					• Todas las separaciones de los miembros del equipo se suman y clasifican.<br><br>
+
+					• En el podio se otorgarán premios a equipos de 4 personas.<br><br>
+
+					• Todos los miembros de un Equipo de Ciclismo son elegibles para premios individuales por grupo de edad.<br><br>
+
+					• No hay una cantidad máxima de miembros por equipo.<br>
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-6 hidden-xs'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+	<div class='gray'></div>	
+
+	<div class="col-xs-12 categorias" style='margin-bottom:10%'>
+		<div class='col-xs-12'>
+			
+			<div class='col-xs-12 col-md-6 '>
+				<h2> CATEGORÍA</h2>
+				<h1>EL-ELLA</h1>
+				<p class='ruta-p'>
+					Los ciclistas hombre y mujer deben llegar a menos de treinta segundos entre sí.<br><br>
+
+					• Como es difícil encontrar dos ciclistas de la misma capacidad y aptitud, se permite empujar, tirar, arrastrar, animar, gritar y cualquier otro medio de trabajo en equipo.<br><br>
+					
+					• La competencia por equipos Él-ELLA consiste de dos categorías de grupos de edad: menos de 90 años y más de 90 años combinados.<br><br>
+
+					Los miembros de un equipo Él-Ella no son elegibles para premios individuales de grupo de edad debido a que en esta categoría se permite la ayuda entre los miembros del equipo en esta categoría.<br>
+				</p>
+			</div>
+
+			<div class ='col-xs-12 col-md-6 hidden-xs'>
+				<img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/ruta.png">
+			</div>
+		</div>
+	</div>
+
+    <!-- Swiper JS -->
+    <script src="js/swiper.min.js"></script>
+
+    <!-- Initialize Swiper -->
+    <script>
+    var swiper = new Swiper('.swiper-container');
+    </script>
+
+<?php include 'footer.php';?>    

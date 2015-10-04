ff5db2ca87eab1d8c116b9287bc94722376810a1
jdflorez39 <jd.florez39@gmail.com>
2015-10-04 11:56:11 -0500

    swiper added to footer

--- footer.php
+++ footer.php
@@ -1,5 +1,6 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
    <script src="/js/bootstrap.min.js"></script> 
+   <script src="/js/swiper.min.js"></script> 
    <script type="text/javascript" src="/js/TimeCircles.js"></script>
    </body>
 </html>

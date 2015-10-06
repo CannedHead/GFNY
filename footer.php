8a7bab1975b6a3a5d12997557180db9f140ffa8c
jdflorez39 <jd.florez39@gmail.com>
2015-10-05 22:42:21 -0500

    rutas css y js corregidas

--- footer.php
+++ footer.php
@@ -1,7 +1,7 @@
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
-   <script src="/gnfy/js/bootstrap.min.js"></script> 
-   <script src="/gnfy/js/swiper.min.js"></script> 
-   <script src="/gnfy/js/smooth-scroll.min.js"></script>
+   <script src="/js/bootstrap.min.js"></script> 
+   <script src="/js/swiper.min.js"></script> 
+   <script src="/js/smooth-scroll.min.js"></script>
    <!-- Initialize Swiper -->
     <script>
     	var swiper = new Swiper('.swiper-container');

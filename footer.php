2f1b8e1d4baea087ee83c2a65e7e494260083244
jdflorez39 <jd.florez39@gmail.com>
2015-10-05 23:18:07 -0500

    error solved

--- footer.php
+++ footer.php
@@ -1,13 +1,13 @@
    </div>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
-   <script src="/gnfy/js/bootstrap.min.js"></script> 
-   <script src="/gnfy/js/swiper.min.js"></script> 
-   <script src="/gnfy/js/smooth-scroll.min.js"></script>
+   <script src="/js/bootstrap.min.js"></script> 
+   <script src="/js/swiper.min.js"></script> 
+   <script src="/js/smooth-scroll.min.js"></script>
    <!-- Initialize Swiper -->
     <script>
     	smoothScroll.init();
     	var swiper = new Swiper('.swiper-container');
     </script>
-   <script type="text/javascript" src="/gnfy/js/TimeCircles.js"></script>
+   <script type="text/javascript" src="/js/TimeCircles.js"></script>
    </body>
 </html>

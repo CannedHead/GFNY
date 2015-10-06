6f7f837de65cbabe8d7d5b698c176b204c0f76a4
jdflorez39 <jd.florez39@gmail.com>
2015-10-06 18:16:55 -0500

    mobile menu

--- footer.php
+++ footer.php
@@ -1,4 +1,5 @@
    </div>
+   </div>
    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script> 
    <script src="/js/bootstrap.min.js"></script> 
    <script src="/js/swiper.min.js"></script> 
@@ -7,6 +8,22 @@
     <script>
     	smoothScroll.init();
     	var swiper = new Swiper('.swiper-container');
+
+      $(function() {
+          $('.toggle-menu').click(function() {
+              toggleMenu();
+          });
+      });
+
+      function toggleMenu() {
+          if ($('#off-canvas-menu').hasClass('menu-open')) { 
+              $('#off-canvas-menu').removeClass('menu-open');
+              $('#off-canvas-menu').addClass('menu-closed');
+          } else {                
+              $('#off-canvas-menu').removeClass('menu-closed');
+              $('#off-canvas-menu').addClass('menu-open');
+          }            
+      }
     </script>
    <script type="text/javascript" src="/js/TimeCircles.js"></script>
    </body>
--- header.php
+++ header.php
@@ -14,6 +14,74 @@
         <link rel="stylesheet" href="/css/style.css">
     </head>
     <body>
+        <div class="menu-wrapper">
+
+        <!-- MOBILE MENU -->
+        <div class="menu-mobile col-xs-12 menu-closed" id="off-canvas-menu">
+          <ul class="list-unstyled menu-options nav navbar-nav navbar-inverse"> 
+              <div class="closecontainer">
+                <a href="#"class="closemenu toggle-menu"><span class="glyphicon glyphicon-remove"></span></a>
+              </div>
+              <li>
+                  <a href="/">HOME</a>
+              </li>
+              <li class="dropdown">
+                  <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">CARRERA <span class="caret"></span></a>
+                  <ul class="dropdown-menu">
+                    <li>
+                      <a id="carrera" href="/carrera/ruta">RUTA</a>
+                    </li>
+                    <li class="rey-reina-link left-inline">
+                      <a class="rey-reina-links" href="/carrera/rey">REY Y REINA <br>DE LA MONTAÑA</a>
+                    </li>
+                    <li class='left-inline' >
+                      <a href="/carrera/reglas">REGLAS</a>
+                    </li>
+                    <li class='left-inline' >
+                      <a href="/carrera/categorias">CATEGORIAS</a>
+                    </li>
+                    <li class='left-inline' >
+                      <a href="/carrera/resultados">RESULTADOS</a>
+                    </li>
+                    <li class='left-inline' >
+                      <a href="/carrera/entrenamiento">ENTRENAMIENTO</a>
+                    </li>
+                    <li class='left-inline' >
+                      <a style='visibility:hidden' href="/carrera/entrenamiento">ENTRENAMIENTO</a>
+                    </li>
+                  </ul>
+              </li>
+              <li class="dropdown">
+                 <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">VIAJE<span class="caret"></span></a>
+                 <ul class="dropdown-menu sub" style='left: -52%; !important'>
+                   <li>
+                     <a href="/viaje/hoteles">HOTELES</a>
+                   </li>
+                   <li class='left-inline'>
+                     <a href="#">CIUDAD</a>
+                   </li>
+                   <li class='left-inline'>
+                     <a href="#">GUÍA TURÍSTICA</a>
+                   </li>
+                   <li class='left-inline'>
+                     <a style='visibility:hidden' href="#">GUÍA TURÍSTICA</a>
+                   </li>             
+                 </ul>
+              </li>
+              <li><a href="#">SHOP</a>
+              </li>
+              <li><a href="#">BLOG</a>
+              </li>
+              <li><a href="#">VIP</a>
+              </li>
+              <li><a href="http://www.gfny.com" target="_blank">GFNY WORLD</a>
+              </li>
+              <li><a href="/contacto">CONTACTO</a>
+              </li>
+          </ul>
+        </div>
+        <!-- END MOBILE MENU -->
+
         <!-- SUBHEADER -->
         <div class="col-xs-12 subheader">
           <div class="pull-right">
@@ -36,8 +104,8 @@
             <img class="img-responsive" src="https://s3-sa-east-1.amazonaws.com/cannedhead.gfny/LOGO+GFNY.svg">
           </div>
           <div class="col-xs-3 menu">
-            <a href="#">MENÚ</a>
-            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+            <a id="menu-btn" class="toggle-menu" href="#">MENÚ</a>
+            <button type="button" class="navbar-toggle collapsed toggle-menu" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
@@ -123,6 +191,8 @@
           </nav>
         </div>
         <!-- END MENU -->
+
+
         <div class="container">
         
 
